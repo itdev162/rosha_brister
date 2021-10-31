f9ac2921108fc89eee49b9f83fbed250a901c997
ShaeB56 <brister3@gmatc.matc.edu>
2021-10-31 14:26:41 -0500

    Fixing and redoing activity-3

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values {get; set;}
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
